72b4f06f15bf7ff44eec84cdd7bcf42b6b2d23f5
Coen Mooij <coen.mooij@werkspot.nl>
2017-10-28 14:39:44 +0200

    add first test

---
+++ tests/SudokuServiceTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CoenMooij\Sudoku\Generator;
+
+use PHPUnit\Framework\TestCase;
+
+class SudokuServiceTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function generatePuzzle(): void
+    {
+        $this->assertTrue(true);
+    }
+}
